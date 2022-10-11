69390574d4d2998630f6b46d223a635cb1a76b43
Milo <65787244+Shiggyyy@users.noreply.github.com>
2022-10-11 21:20:02 +0200

    Add files via upload

--- MultiTool.py
+++ MultiTool.py
@@ -3,19 +3,11 @@ import os
 import json
 from time import sleep
 import requests
-import platform
 
 
-os_name = platform.system()
 config_path = os.getenv("APPDATA")
 download_config = "https://raw.githubusercontent.com/Shiggyyy/MultiTool/main/config.json"
-download_user = "https://raw.githubusercontent.com/Shiggyyy/MultiTool/main/user.json"
-update_file = "https://raw.githubusercontent.com/Shiggyyy/MultiTool/main/UpdateHandler.py"
-
-if os_name == "Windows":
-    subprocess.check_output(f"setx MultiTool {os.getcwd()}", shell=True)
-else:
-    exit()
+update_file = "https://raw.githubusercontent.com/Shiggyyy/MultiTool/main/UpdateHandler.exe"
 
 def checkforupdate():
     download = requests.get("https://raw.githubusercontent.com/Shiggyyy/MultiTool/main/update.json")
@@ -29,16 +21,16 @@ def checkforupdate():
         if version == update:
             print("Login Manager")
         else:
-            if os.path.exists(config_path + "/MultiTool/UpdateHandler.py"):
-                os.system("cd" + config_path + "py /MultiTool/UpdateHandler.py")
-                print("Hab update gemacht !")
+            if os.path.exists(config_path + "/MultiTool/UpdateHandler.txt"):
+                subprocess.call(config_path + "/MultiTool/UpdateHandler.exe")
+                print("Hab update gemacht ! ")
             else:
                 print("Downlaod update path !")
                 download_update = requests.get(update_file, allow_redirects=True)
                 join_path = os.path.join(config_path)
-                open(join_path + "/MultiTool/UpdateHandler.py", "wb").write(download_update.content)
+                open(join_path + "/MultiTool/UpdateHandler.exe", "wb").write(download_update.content)
                 print("Update Downloaded !")
-                os.system("cd" + config_path + "py /MultiTool/UpdateHandler.py")
+                subprocess.call(config_path + "/MultiTool/UpdateHandler.exe")
                 exit()
     else:
         setup()      
@@ -47,35 +39,24 @@ def checkforupdate():
 def setup():
     config_folder = "MultiTool"
     path = os.path.join(config_path, config_folder)
-    if not os.path.exists(config_path + "/MultiTool"):
+    if not os.path.exists(config_path + "\MultiTool"):
         os.mkdir(path)
         sleep(3)
-        downloadconfig = requests.get(download_config, allow_redirects=True)
-        open(config_path + "/MultiTool/config.json", "wb").write(downloadconfig.content)
-    else:
-        pass
-
-    user_folder = "user"
-    pathuser = os.path.join(config_path + "/MultiTool",  user_folder)    
-
-    if not os.path.exists(pathuser + "/MultiTool/user"):
-        try:
-            os.mkdir(pathuser)
-        except FileExistsError:
-            pass
-        sleep(3)
-        downloaduser = requests.get(download_user, allow_redirects=True)
-        open(config_path + "/MultiTool/user/user.json", "wb").write(downloaduser.content)
+        download = requests.get(download_config, allow_redirects=True)
+        open(config_path + "\MultiTool\config.json", "wb").write(download.content)
     else:
         pass
 
 
 
 try:
-    with open(config_path + "/MultiTool/config.json", "r") as file:
+    with open(config_path + "\MultiTool\config.json", "r") as file:
         config = json.load(file)
 
     print("[SYSTEM] Config Loaded !")
+
+    login = config["LOGIN_NAME"]
+    passwort = config["LOGIN_PASSWORT"]
     version = config["VERSION_CONFIG"]
 
     checkforupdate()
--- UpdateHandler.py
+++ UpdateHandler.py
@@ -1,28 +1,16 @@
 import os
-import shutil
 import requests
-from time import sleep
-config_path = os.getenv("APPDATA")
 
-MultiTool = "https://raw.githubusercontent.com/Shiggyyy/MultiTool//main/MultiTool.py"
+config_path = os.getenv("APPDATA")
+MultiTool = "https://raw.githubusercontent.com/Shiggyyy/MultiTool/main/MultiTool.exe"
 config = "https://raw.githubusercontent.com/Shiggyyy/MultiTool/main/config.json"
-exefile = "MultiTool"
 
 def update():
     downloadconfig = requests.get(config, allow_redirects=True)
     open("config.json", "wb").write(downloadconfig.content)
 
+    print("Config Downloaded und settet !")
+
     downloadMultiTool = requests.get(MultiTool, allow_redirects=True)
-    open("MultiTool.py", "wb").write(downloadMultiTool.content)
-    installpath = os.getenv("MultiTool")
-
-
-    sleep(3)
-    shutil.move(exefile, installpath)
-
-
-update()
-
-
-
+    open("MultiTool", "wb").write(downloadMultiTool.content)
 
